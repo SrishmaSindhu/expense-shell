01210b6935d4e58611ce43a26f78847b97958076
Sindhuja Addala <sindhu.a@saince.in>
2024-04-19 21:49:43 +0530

    mysql

--- backend.sh
+++ backend.sh
@@ -9,6 +9,10 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+echo "please enter db password"
+read -s Mysql_root_password
+
+
 if [ $USERID -ne 0 ]
 then 
     echo "please run the script with root access"
@@ -37,27 +41,52 @@ VALIDATE() {
   VALIDATE $? "installing nodejs"
 
 
-  id expense
+  id expense &>>$LOGFILE
   if [$? -ne 0]
   then 
-    useradd expense
+    useradd expense &>>$LOGFILE
     VALIDATE $? "Creating expense user"
   else
     echo  -e "User already exists.. $Y SKIPPING $N"
   fi  
 
-  mkdir -p /app
+  mkdir -p /app &>>$LOGFILE
   VALIDATE $? "Creating App directive"
 
   curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
   VALIDATE $? "Downloading backend code"
 
-  cd /app
+  cd /app &>>$LOGFILE
   unzip /tmp/backend.zip
   VALIDATE $? "extracted backend code"
 
-  npm install
+  npm install &>>$LOGFILE
   VALIDATE $? "installing node js dependencies"
+
+  cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+  VALIDATE $? "copied backend service"
+
+  systemctl daemon-reload &>>$LOGFILE
+  VALIDATE $? "Demon reload"
+
+  systemctl start backend &>>$LOGFILE
+  VALIDATE $? "start backend"
+
+  systemctl enable backend &>>$LOGFILE
+  VALIDATE $? "enabling backend"
+
+  dnf install mysql -y &>>$LOGFILE
+  VALIDATE $? "installing mysql client"
+
+
+  #below code used for idompotent in nature
+  mysql -h db-shellscript.sdevops.store -uroot -p${Mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+  VALIDATE $? "Schema loading"
+
+  systemctl restart backend &>>$LOGFILE
+  VALIDATE $? "restarting backend"
+
+
 
 
 
